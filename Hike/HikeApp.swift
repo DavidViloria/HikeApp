deb7e0bf45178959637f6e5535974d629c790161
David Viloria Ortega <david.viloria0424@gmail.com>
2024-08-21 11:17:37 -0500

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by David Viloria Ortega on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
