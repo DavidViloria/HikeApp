3aa2cbc6e610e445b68277acb6d12b488298b928
David Viloria Ortega <david.viloria0424@gmail.com>
2024-08-23 00:56:07 -0500

    CardView Header

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -11,13 +11,43 @@ struct CardView: View {
     var body: some View {
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topTrailing, endPoint: .bottomLeading))
-                    .frame(width: 256, height: 256)
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+            VStack {
+                //Header
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size:52))
+                            .foregroundStyle(
+                                LinearGradient(colors: [.customGrayLight, .customGrayMediumm],
+                                               startPoint: .bottom,
+                                               endPoint: .top)
+                        )
+                        Spacer()
+                        Button(action: {
+                            
+                        }, label: {
+                            CustomButtonView()
+                        })
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and family")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundStyle(.colorGrayMedium)
+                        
+                }
+                .padding(.horizontal,30)
+                //Main Content
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topTrailing, endPoint: .bottomLeading))
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                //Footer
             }
         }
         .frame(width: 320, height: 520)
